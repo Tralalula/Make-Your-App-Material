415a29492f53f40a686369fa4eb02f263eac114d
Tobias <qenzoz@gmail.com>
2016-02-15 02:38:08 +0100

    Initial commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
@@ -0,0 +1,79 @@
+package com.example.xyzreader.remote;
+
+import android.util.Log;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONTokener;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class RemoteEndpointUtil {
+    private static final String TAG = "RemoteEndpointUtil";
+
+    private RemoteEndpointUtil() {
+    }
+
+    public static JSONArray fetchJsonArray() {
+        String itemsJson = null;
+        try {
+            itemsJson = fetchPlainText(Config.BASE_URL);
+        } catch (IOException e) {
+            Log.e(TAG, "Error fetching items JSON", e);
+            return null;
+        }
+
+        // Parse JSON
+        try {
+            JSONTokener tokener = new JSONTokener(itemsJson);
+            Object val = tokener.nextValue();
+            if (!(val instanceof JSONArray)) {
+                throw new JSONException("Expected JSONArray");
+            }
+            return (JSONArray) val;
+        } catch (JSONException e) {
+            Log.e(TAG, "Error parsing items JSON", e);
+        }
+
+        return null;
+    }
+
+    static String fetchPlainText(URL url) throws IOException {
+        return new String(fetch(url), "UTF-8");
+    }
+
+    static byte[] fetch(URL url) throws IOException {
+        // Solution to getting images to show
+        // Gotten from https://discussions.udacity.com/t/volley-not-loading-images-from-https-on-device/37286/16
+        InputStream in = null;
+
+        try {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            Response response = client.newCall(request).execute();
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            in = new ByteArrayInputStream(response.body().string().getBytes());
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, bytesRead);
+            }
+            return out.toByteArray();
+
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+}
